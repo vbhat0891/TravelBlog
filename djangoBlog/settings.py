951f5726616dd02afd9f9922a7a3b4fbba9953f0
Vivek Bhat <vbhathosmat@unomaha.edu>
2018-01-28 15:41:51 -0600

    updated views.py

--- djangoBlog/settings.py
+++ djangoBlog/settings.py
@@ -27,10 +27,6 @@ DEBUG = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
-try:
-    from .local_settings import *
-except ImportError:
-    pass
 
 
 
@@ -129,7 +125,7 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
@@ -151,7 +147,11 @@ os.path.join(PROJECT_ROOT, 'static'),
 
 STATIC_URL = '/static/'
 
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-DATABASES['default'] = dj_database_url.config()
